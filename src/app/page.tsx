33cb262c3476d107b661b6e3e9b19d0c1a8889f7
Vishvam <vishusp77@gmail.com>
2025-12-04 21:46:59 +0530

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,8 @@ import {
   saveResponsiveLayoutsToStorage,
   convertLayoutsToStorageFormat,
   convertStorageFormatToLayouts,
+  exportDashboardConfig,
+  importDashboardConfig,
   type BreakpointLayouts,
   type ImportValidationResult,
 } from '@/utils/persistence';
